260a89c7ed68a4b38b90e457b14e69cca906ec3f
Michael Mentele <Michaelrmentele@gmail.com>
2016-07-10 09:32:27 -0700

    updated readme

---
+++ README.md
@@ -0,0 +1,5 @@
+A simple note appending app with a simple GUI that is really only useful on Windows. It basically allows you to append notes to 'buckets' or files of related notes using hashtags. 
+
+This can be easily replace in Linux/Unix environments with a single line of bash script. 
+
+I may modify it as a CLI bash utility, but we will see.
--- noteParser.py
+++ noteParser.py
@@ -1,5 +1,6 @@
 #noteParser.py
 '''
+3.5.x
 Description: This module splits a note up into its #tag and its text.
 Author: Michael Mentele
 '''
@@ -9,7 +10,7 @@ def parseText(note):
 	'''Parses a string into its hashtag and textstring.'''
 	try:
 		hashtag_idx = note.index('#')
-	except ValueError, e:
+	except ValueError:
 		raise Exception('No hashtag in note!')
 
 	hashtag = note[hashtag_idx:]
